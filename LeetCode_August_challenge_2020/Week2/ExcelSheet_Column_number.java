9490d72ce89b51f683716dfb4a1eae571cd29ad4
Manmeet2018 <manietdavv@gmail.com>
2020-08-10 13:26:46 +0530

    feat: Changed solution to ATOI

--- LeetCode_August_challenge_2020/Week2/ExcelSheet_Column_number.java
+++ LeetCode_August_challenge_2020/Week2/ExcelSheet_Column_number.java
@@ -1,19 +1,9 @@
 class Solution {
-    private int pow(int a, int b) {
-        int ans = 1;
-        while(b > 0) {
-            ans *= a;
-            --b;
-        }
-        return ans;
-    }
     public int titleToNumber(String s) {
         int size = s.length();
         int ans = 0;
         for(int i=0; i<size; ++i) {
-            int num = pow(26, size-i-1);
-            int b = s.charAt(i) - 'A' + 1;
-            ans += (num*b);
+            ans = ans*26 + s.charAt(i) - 'A' + 1;
         }
         return ans;
     }
