de1925e12d1331b7289ddd203325c1683472d2b3
Manmeet2018 <manietdavv@gmail.com>
2020-08-12 14:26:51 +0530

    Feat: optimized the TC

--- LeetCode_August_challenge_2020/Week2/Pascal_Triangle_2nd.cpp
+++ LeetCode_August_challenge_2020/Week2/Pascal_Triangle_2nd.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<int> getRow(int k) {
-        vector<long long> ans(k+1);
+        vector<int> ans(k+1);
         ans[0] = 1;
         for(int i=1; i<=k; ++i) {
-        ans[i] = ans[i-1]*(k-i+1)/i;
+        ans[i] = (int)((long)ans[i-1]*(k-i+1)/i);
         }
-        vector<int> res;
-        for(auto i : ans)
-            res.push_back(i);
-        return res;
+        return ans;
     }
 };
